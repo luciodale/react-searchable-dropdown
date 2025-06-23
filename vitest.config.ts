4540608db410aa4f7626c64f5cdc4cb572807735
Lucio D'Alessandro <lda@juxt.pro>
2025-06-23 20:01:19 +0200

    Add testing pipeline with GitHub Actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		include: ["src/**/*.test.ts"],
+		environment: "jsdom",
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "html", "lcov"],
+			exclude: ["node_modules/", "dist/", "**/*.d.ts", "**/*.config.*", "**/coverage/**"],
+		},
 	},
 });
